71fbe01c5787f15f61dc5d29dcefaa262b337dfb
Sanjay-ES <sanjay.e@strat-agile.com>
2024-02-17 17:58:50 +0530

    banner- and great ssec

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,52 @@
 
-// 
+// input menu
 const incrementButton = document.querySelector("#increment");
 const decrementButton = document.querySelector("#decrement");
 const quantityInput = document.querySelector("#quantity");
 
-incrementButton.addEventListener("click", () => {
-  quantityInput.value = parseInt(quantityInput.value) + 1;
-});
-decrementButton.addEventListener("click", () => {
-  quantityInput.value = parseInt(quantityInput.value) - 1;
+if(quantityInput){
+  incrementButton.addEventListener("click", () => {
+    quantityInput.value = parseInt(quantityInput.value) + 1;
+  });
+  decrementButton.addEventListener("click", () => {
+    quantityInput.value = parseInt(quantityInput.value) - 1;
+  });
+}
+
+// banner
+$('.vm-banner__outer').slick({
+  lazyLoad: 'ondemand',
+  dots: true,
+  infinite: false,
+  speed: 1300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
